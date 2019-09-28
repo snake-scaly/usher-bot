ad8df6bc4ffdb19efd2381cab6e5874099a28254
Aguacero11 <delete2210@mail.ru>
2019-09-28 23:29:39 +0800

    initial commit

--- usherBot.js
+++ usherBot.js
@@ -1,7 +1,5 @@
 require('dotenv').config()
 
-require("http").createServer(async (req,res) => { res.statusCode = 200; res.write("ok"); res.end(); }).listen(3000, () => console.log("Now listening on port 3000"));
-
 const discord = require ('discord.js');
 
 var client = new discord.Client();
